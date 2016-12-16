92c2aa8092c4d7f454c35146a13ddb1088047273
utrobin <patr.rus@gmail.com>
2016-12-16 16:27:47 +0300

    РК4 (#2)
    * Поправил игру без регистрации
    
    * Добавил возможность попадания
    
    * Сверстал ошибку 404, поправил верстку на всех страницах.
    
    * Добавил конфиг Travis
    
    * Поправил конфиг трэвиса
    
    * Поправил конфиг трэвиса
    
    * Поправил конфиг трэвиса
    
    * Поправил конфиг трэвиса
    
    * Поправил конфиг трэвиса
    
    * Поправил конфиг трэвиса
    
    * Поправил конфиг трэвиса
    
    * Поправил конфиг трэвиса
    
    * Поправил конфиг трэвиса
    
    * Поправил конфиг трэвиса
    
    * Поправил конфиг трэвиса
    
    * Поправил конфиг трэвиса
    
    * Поправил конфиг трэвиса

--- public/tools/isAuth.js
+++ public/tools/isAuth.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { auth, login } from '../actions/User';
+import fetch from 'isomorphic-fetch';
 
 const isAuth = (dispatch) => {
   fetch('/api/isauth', {
@@ -8,6 +9,8 @@ const isAuth = (dispatch) => {
   })
     .then(response => {return response.json()})
     .then(data => {
+      document.querySelector('.main-preloader').style.display = 'none';
+
       let user = localStorage.getItem('user');
       if (user !== undefined) {
         dispatch(login(JSON.parse(data)));
